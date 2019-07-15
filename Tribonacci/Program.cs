c51c847e4e2361739f631f346679cf2edba05138
Павел Музыченко <pavelmuzychenkokpi@gmail.com>
2019-07-15 22:25:31 +0300

    Ready

---
+++ Tribonacci/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tribonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            (int n, int k) = FetchInput();
+            ulong[] result = GetPart(n, k);
+
+            foreach (var item in result)
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.ReadKey();
+        }
+
+        static (int, int) FetchInput()
+        {
+            int n, k;
+
+            do
+            {
+                Console.WriteLine("Enter N:");
+                bool flag = int.TryParse(Console.ReadLine(), out n);
+                if (!flag)
+                {
+                    Console.WriteLine("N must be an integer");
+                    continue;
+                }
+                if (n < 1)
+                {
+                    Console.WriteLine("N must be greater than 0");
+                    continue;
+                }
+
+                Console.WriteLine("Enter K:");
+                flag = int.TryParse(Console.ReadLine(), out k);
+                if (!flag)
+                {
+                    Console.WriteLine("K must be an integer");
+                    continue;
+                }
+                if (k < 2)
+                {
+                    Console.WriteLine("K must be greater than 1");
+                    continue;
+                }
+
+                if (k <= n)
+                {
+                    Console.WriteLine("K must be greater than N");
+                    continue;
+                }
+
+                break;
+
+            } while (true);
+
+            return (n, k);
+        }
+
+        static ulong[] GetPart(int n, int k)
+        {
+            List<ulong> numbers = new List<ulong>();
+            Dictionary<int, ulong> cache = new Dictionary<int, ulong>();
+            for (int i = n; i <= k; i++)
+            {
+                numbers.Add(Tribonacci(i, cache));
+            }
+
+            return numbers.ToArray();
+        }
+
+        static ulong Tribonacci(int x, Dictionary<int, ulong> cache)
+        {
+            if (cache.TryGetValue(x, out ulong result))
+                return result;
+
+            result = x > 3 ? Tribonacci(x - 1, cache) + Tribonacci(x - 2, cache) + Tribonacci(x - 3, cache) : 1;
+            cache[x] = result;
+            return result;
+        }
+    }
+}
